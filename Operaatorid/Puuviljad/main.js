6ece21405c6144dbe9caf8fe3193aff76b477f1a
Jorven-Tsahkna-TA-23A <144696174+Jorven-Tsahkna-TA-23A@users.noreply.github.com>
2023-09-18 15:40:39 +0300

    puuviljad

---
+++ Operaatorid/Puuviljad/main.js
@@ -0,0 +1,19 @@
+var õunad = 0;
+var pirnid = 0;
+
+while (true) { 
+  var puuvili = prompt();
+
+  if (puuvili == "õun") {
+    õunad++;
+    console.log("Õuni on: " + õunad)
+  }
+  else if (puuvili == "pirn") {
+    pirnid++;
+    console.log("Pirne on: " + pirnid)
+  } 
+  else {
+    console.log("Tundmatu puuvili");
+    break
+  }
+}
